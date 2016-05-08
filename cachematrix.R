b7146eff9fa5e93be827df185c16c9a2a6cdb7f0
David Alexander <david.alexander@continentalmills.com>
2016-05-08 14:08:27 -0700

    Modified function template for the R Programming week 3 assignement.
    Includes a function to cache a calculated inverse matrix solution
    and a function to look for the cached value.  The result is retrieved
    if available.  Otherwise, the solution is calculated and stored.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to calculate, store and retrieve an inverse matrix solution
+## Created by David Alexander for the R Programming Week 3 Assignment.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates storate to calculate the inverse of a matrix
+## and stores it as m for later retrieval outside of the function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    set_inverseMatrix <- function(solve) m <<- solve
+    get_inverseMatrix <- function() m
+    list(set = set, get = get,
+         set_inverseMatrix = set_inverseMatrix,
+         get_inverseMatrix = get_inverseMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve uses the solve function to find the inverse of a
+## matrix (square only).  If the function has already been run
+## and the solution stored in cache, the cached value is returned.
+## Otherwise, the solution is calculated and stored for future retrieval.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$get_inverseMatrix()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    matrix.data <- x$get()
+    m <- solve(matrix.data, ...)
+    x$set_inverseMatrix(m)
+    return(m)
 }
